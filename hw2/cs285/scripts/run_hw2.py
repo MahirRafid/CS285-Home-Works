5dcd1720db1172fe49af87187768617fa9e4fcf4
mahir_05 <mahirmosleh136@gmail.com>
2026-02-16 22:39:27 +0600

    fix : complete the TODO section(s) upto trajectory sampling

--- hw2/cs285/scripts/run_hw2.py
+++ hw2/cs285/scripts/run_hw2.py
@@ -70,7 +70,10 @@ def run_training_loop(args):
         print(f"\n********** Iteration {itr} ************")
         # TODO: sample `args.batch_size` transitions using utils.sample_trajectories
         # make sure to use `max_ep_len`
-        trajs, envsteps_this_batch = None, None  # TODO
+        trajs, envsteps_this_batch = utils.sample_trajectories(env=env, 
+                                                               policy=agent, 
+                                                               min_timesteps_per_batch=args.batch_size, 
+                                                               max_length=max_ep_len)  # TODO
         total_envsteps += envsteps_this_batch
 
         # trajs should be a list of dictionaries of NumPy arrays, where each dictionary corresponds to a trajectory.
